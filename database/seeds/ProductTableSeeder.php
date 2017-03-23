ad9faa4d649edfaa3a48a9f3d6f6c7db078da17c
Søren Spangsberg Jørgensen <smsj@easv.dk>
2017-03-23 14:10:24 +0100

    Connected app to DB and outputting data

--- database/seeds/ProductTableSeeder.php
+++ database/seeds/ProductTableSeeder.php
@@ -14,41 +14,41 @@ class ProductTableSeeder extends Seeder
         $product = new \App\Product([
             'imagePath' => 'https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Homm3boxart.jpg/256px-Homm3boxart.jpg',
             'title' => 'Heroes of Might and Magic 3',
-            'description' => 'Super cool game - played 1000+ of hours',
+            'description' => 'Super cool turn-based strategy game - played 1000+ of hours',
             'price' => 10
         ]);
         $product->save();
 
 
         $product = new \App\Product([
-            'imagePath' => 'https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Homm3boxart.jpg/256px-Homm3boxart.jpg',
-            'title' => 'Heroes of Might and Magic 3',
-            'description' => 'Super cool game - played 1000+ of hours',
-            'price' => 10
+            'imagePath' => 'https://upload.wikimedia.org/wikipedia/en/7/77/HoMM3_Shadow_of_Death_box_art.jpg',
+            'title' => 'Heroes of Might and Magic 3 - Shadow of Death',
+            'description' => 'Expansion pack to HOMM3 turn-based strategy game',
+            'price' => 7
         ]);
         $product->save();
 
         $product = new \App\Product([
-            'imagePath' => 'https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Homm3boxart.jpg/256px-Homm3boxart.jpg',
-            'title' => 'Heroes of Might and Magic 3',
-            'description' => 'Super cool game - played 1000+ of hours',
-            'price' => 10
+            'imagePath' => 'http://settlers2.net/wp-content/uploads/2011/07/The-Settlers-II-Veni-Vidi-Vici-C.jpg',
+            'title' => 'The Settlers II',
+            'description' => 'Nice strategy game of the world dominance',
+            'price' => 12
         ]);
         $product->save();
 
         $product = new \App\Product([
-            'imagePath' => 'https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Homm3boxart.jpg/256px-Homm3boxart.jpg',
-            'title' => 'Heroes of Might and Magic 3',
-            'description' => 'Super cool game - played 1000+ of hours',
-            'price' => 10
+            'imagePath' => 'https://gamemusic.files.wordpress.com/2007/12/warcraft_orcs_humans-400-400.jpg?w=1400',
+            'title' => 'WarCraft',
+            'description' => 'First version of the era making strategy game',
+            'price' => 11
         ]);
         $product->save();
 
         $product = new \App\Product([
-            'imagePath' => 'https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Homm3boxart.jpg/256px-Homm3boxart.jpg',
-            'title' => 'Heroes of Might and Magic 3',
-            'description' => 'Super cool game - played 1000+ of hours',
-            'price' => 10
+            'imagePath' => 'http://www.mobygames.com/images/covers/l/146326-starcraft-anthology-windows-other.jpg',
+            'title' => 'StarCraft',
+            'description' => 'Cool strategy game with stars and galaxy theme',
+            'price' => 15
         ]);
         $product->save();
 
--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -5,47 +5,23 @@
 @endsection
 
 @section('content')
-    <div class="row">
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-                <img src="https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Homm3boxart.jpg/256px-Homm3boxart.jpg" class="img-responsive" alt="...">
-                <div class="caption">
-                    <h3>Product Title</h3>
-                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa inventore magnam officia pariatur quaerat quisquam veritatis. Alias eaque facilis, fugiat iusto minima nemo nobis non officiis provident quae, sit temporibus.</p>
-                    <div class="clearfix">
-                        <div class="pull-left price">$12</div>
-                        <a href="#" class="btn btn-success pull-right price" role="button">Add to Cart</a>
+    @foreach($products->chunk(3) as $productChunk)
+        <div class="row">
+            @foreach($productChunk as $product)
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <img src="{{ $product->imagePath }}" class="img-responsive" alt="...">
+                        <div class="caption">
+                            <h3>{{ $product->title }}</h3>
+                            <p class="description">{{ $product->description }}</p>
+                            <div class="clearfix">
+                                <div class="pull-left price">${{ $product->price }}</div>
+                                <a href="#" class="btn btn-success pull-right price" role="button">Add to Cart</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
+            @endforeach
         </div>
-
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-                <img src="https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Homm3boxart.jpg/256px-Homm3boxart.jpg" class="img-responsive" alt="...">
-                <div class="caption">
-                    <h3>Product Title</h3>
-                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa inventore magnam officia pariatur quaerat quisquam veritatis. Alias eaque facilis, fugiat iusto minima nemo nobis non officiis provident quae, sit temporibus.</p>
-                    <div class="clearfix">
-                        <div class="pull-left price">$12</div>
-                        <a href="#" class="btn btn-success pull-right price" role="button">Add to Cart</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-                <img src="https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Homm3boxart.jpg/256px-Homm3boxart.jpg" class="img-responsive" alt="...">
-                <div class="caption">
-                    <h3>Product Title</h3>
-                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa inventore magnam officia pariatur quaerat quisquam veritatis. Alias eaque facilis, fugiat iusto minima nemo nobis non officiis provident quae, sit temporibus.</p>
-                    <div class="clearfix">
-                        <div class="pull-left price">$12</div>
-                        <a href="#" class="btn btn-success pull-right price" role="button">Add to Cart</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    @endforeach
 @endsection
